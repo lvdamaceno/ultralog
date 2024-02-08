dcbe01eb729c3b126aaf7639ff54301b7d984c1f
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2024-02-08 19:19:52 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,214 @@
-import Image from "next/image";
+import {
+  MapPinLine,
+  LinkedinLogo,
+  InstagramLogo,
+  FacebookLogo,
+  WhatsappLogo,
+  GlobeHemisphereWest,
+  Package,
+  Timer,
+  CaretDoubleRight,
+  Handshake,
+} from '@phosphor-icons/react/dist/ssr';
+import Image from 'next/image';
+import logo from '/src/app/assets/logo.png';
+import road from '/src/app/assets/warehouse.avif';
+import boxes from '/src/app/assets/boxes.avif';
+import shipping from '/src/app/assets/shipping.avif';
+import fast from '/src/app/assets/fast.avif';
+import location from '/src/app/assets/location.avif';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className=" border-t-4 border-orange-600">
+      {/* header */}
+      <div className="header flex justify-between p-2 max-w-4xl m-auto">
+        <div className="logo flex items-center">
+          <Image src={logo} alt="Logo" quality={50} width={75} />
+          <p className="text-4xl">ultra.log.br</p>
+        </div>
+        <div className="side-menu flex gap-5 pr-5">
+          <div className="flex items-center gap-2">
+            <a
+              href="https://parceiros.ultra.log.br/"
+              className="flex items-center gap-2 "
+            >
+              <Handshake size={32} color=" #ea580c" weight="fill" />
+
+              <p className="uppercase">Seja nosso parceiro</p>
+            </a>
+          </div>
+          <div className="flex items-center gap-2">
+            <a
+              href="https://cademeupedido.fusiondms.com.br/"
+              className="flex items-center gap-2 "
+            >
+              <MapPinLine size={32} color=" #ea580c" weight="fill" />
+
+              <p className="uppercase">Rastreie seu pedido</p>
+            </a>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      {/* menu */}
+      <div className="menu justify-between bg-orange-600 p-5 text-white">
+        <div className="menu-itens max-w-4xl m-auto flex justify-between items-center ">
+          <a href="">HOME</a>
+          <a href="">QUEM SOMOS</a>
+          <a href="">NOSSOS SERVIÇOS</a>
+          <a href="">ÁREA DE ABRANGÊNCIA</a>
+          <div className="socials flex gap-2">
+            <a href="">
+              <LinkedinLogo size={32} color=" #fff" weight="fill" />
+            </a>
+            <a href="">
+              <InstagramLogo size={32} color=" #fff" weight="fill" />
+            </a>
+            <a href="">
+              <FacebookLogo size={32} color=" #fff" weight="fill" />
+            </a>
+            <a href="">
+              <WhatsappLogo size={32} color=" #fff" weight="fill" />
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* banner */}
+      <div className="banner  h-96 overflow-hidden items-center object-cover relative">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={road}
+          alt="trucks"
+          quality={100}
+          fill
+          style={{ objectFit: 'cover' }}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="servicos max-w-4xl m-auto">
+        <h1 className="uppercase my-10 text-center text-2xl">
+          nossos principais serviços
+        </h1>
+        <div className="cards flex justify-between gap-5 pb-5">
+          <div className="card border flex flex-col gap-3 rounded-lg overflow-hidden p-2">
+            <Image src={boxes} alt="trucks" quality={50} />
+            <h1 className="uppercase text-2xl">P2P - SHIP FROM STORE</h1>
+            <p className="text-justify">
+              Logística direcionada para produtos que tem sua expedição direto
+              do Centro de Distribuição diretamente para o consumidor final com
+              a entrega podendo ser realizada até no mesmo dia.
+            </p>
+            <button className="uppercase p-2 bg-orange-600">saiba mais</button>
+          </div>
+          <div className="card flex border p-2 flex-col gap-3 rounded-lg overflow-hidden">
+            <Image src={shipping} alt="trucks" quality={50} />
+            <h1 className="uppercase text-2xl">SAME DAY</h1>
+            <p className="text-justify">
+              Logística voltada para entrega rápida de produtos. Nosso objetivo
+              é entregar no menor prazo possível. Indicado para coletas até 14h
+              e entrega no mesmo dia até 22h com regiões a definir sob consulta.
+            </p>
+            <button className="uppercase p-2 bg-orange-600">saiba mais</button>
+          </div>
+          <div className="card flex border p-2 flex-col gap-3 rounded-lg overflow-hidden">
+            <Image src={fast} alt="trucks" quality={50} />
+            <h1 className="uppercase text-2xl">ultra EXPRESs</h1>
+            <p className="text-justify">
+              Sempre buscando estar à frente e com inovação estamos
+              constantemente buscando melhorias de prazos em toda a nossa
+              abrangência. Atuamos em todo a região metropolitana de Belém.
+            </p>
+            <button className="uppercase p-2 bg-orange-600">saiba mais</button>
+          </div>
+        </div>
       </div>
-    </main>
+      {/* atendimento */}
+      <div className="atendimento  pt-5 ">
+        <div className="local max-w-4xl m-auto text-center pb-10">
+          <h1 className="uppercase text-2xl pb-5">Área de abrangência</h1>
+          <h2 className="pb-10">
+            Estrategicamente distribuídas, nossas Unidades nos possibilitam
+            rapidez e versatilidade nas operações de last mile, transferências e
+            logística reversa atendendo mais de 2550 municípios em 21 estados do
+            Brasil.
+          </h2>
+          <div className="image overflow-hidden rounded-xl">
+            <Image src={location} alt="Logo" quality={50} />
+          </div>
+        </div>
+        <div className="dados pb-10 bg-orange-600 p-5">
+          <div className="max-w-4xl m-auto flex justify-between">
+            <div className="border p-20 flex flex-col gap-5 items-center text-center text-2xl uppercase rounded-lg text-white">
+              <GlobeHemisphereWest size={128} color="#fff" weight="fill" />
+              <p>
+                255 <br /> cidades <br />
+                atendidas
+              </p>
+            </div>
+            <div className="border p-20 flex flex-col gap-5 items-center text-center text-2xl uppercase rounded-lg text-white">
+              <Package size={128} color="#fff" weight="fill" />
+              <p>
+                8000 <br /> entregas <br />
+                diarias
+              </p>
+            </div>
+            <div className="border p-20 flex flex-col gap-5 items-center text-center text-2xl uppercas rounded-lg text-white">
+              <Timer size={128} color="#fff" weight="fill" />
+              <p>
+                8000 <br /> entregas <br />
+                diarias
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="institucional max-w-4xl m-auto py-5 ">
+        <div className="flex justify-between">
+          <div className="logo">
+            <Image src={logo} alt="Logo" quality={50} />
+          </div>
+          <div className="right border-l-2 border-solid border-orange-600 px-4">
+            <h1 className="uppercase pb-2 font-bold">institucional</h1>
+            <span className="flex items-center uppercase cursor-pointer text-sm pb-1">
+              <CaretDoubleRight /> Home
+            </span>
+            <span className="flex items-center uppercase cursor-pointer text-sm pb-1">
+              <CaretDoubleRight /> quem somos
+            </span>
+            <span className="flex items-center uppercase cursor-pointer text-sm pb-1">
+              <CaretDoubleRight /> nossos serviços
+            </span>
+            <span className="flex items-center uppercase cursor-pointer text-sm pb-1">
+              <CaretDoubleRight /> abrangência
+            </span>
+          </div>
+          <div className="center border-l-2 border-solid border-orange-600 px-4">
+            <h1 className="uppercase pb-2 font-bold">pedidos</h1>
+            <span className="flex items-center uppercase cursor-pointer text-sm">
+              <CaretDoubleRight /> rastrear pedido
+            </span>
+            <h1 className="uppercase pb-2 pt-4 font-bold">redes sociais</h1>
+            <span className="flex items-center uppercase cursor-pointer">
+              <div className="socials flex">
+                <LinkedinLogo size={32} color=" #ea580c" weight="fill" />
+                <InstagramLogo size={32} color=" #ea580c" weight="fill" />
+                <FacebookLogo size={32} color=" #ea580c" weight="fill" />
+                <WhatsappLogo size={32} color=" #ea580c" weight="fill" />
+              </div>
+            </span>
+          </div>
+          <div className="left border-l-2 border-solid border-orange-600 px-4">
+            <h1 className="uppercase pb-2 font-bold">endereço</h1>
+            <a
+              href="https://maps.app.goo.gl/PJ78d7ShM9ELnuU87"
+              className="uppercase text-sm"
+            >
+              Tv. Santa Maria, 47, Atalaia <br /> Ananindeua/PA
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="rodape bg-orange-600 p-8 text-white">
+        <h1>ultra.log.br | Todos os direitos reservados</h1>
+      </div>
+    </div>
   );
 }
