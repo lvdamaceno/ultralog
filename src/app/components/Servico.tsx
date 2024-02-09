e937eb06ce84b65c417f6c9fd98236d4e2f48216
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2024-02-09 12:24:32 -0300

    update servico.tsx

--- src/app/components/Servico.tsx
+++ src/app/components/Servico.tsx
@@ -4,7 +4,7 @@ export default function Servico(props: any) {
   return (
     <div className="card border flex flex-col gap-2 rounded-lg overflow-hidden p-2 hover:shadow-2xl w-full">
       <div className="w-full rounded-lg ">
-        <Image src={props.src} alt="image" quality={50} object-fill />
+        <Image src={props.src} alt="image" quality={50} />
       </div>
       <div className=" flex flex-col gap-2">
         <h1 className="uppercase text-2xl h-6 text-center">{props.title}</h1>
