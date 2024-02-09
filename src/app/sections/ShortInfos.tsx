fb89aead62da4286af04a23ec6187bbabd3f513a
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2024-02-09 15:51:32 -0300

    update ShortInfos

--- src/app/sections/ShortInfos.tsx
+++ src/app/sections/ShortInfos.tsx
@@ -6,7 +6,7 @@ import {
 
 export default function ShortInfos(props: any) {
   return (
-    <div className="dados pb-10 bg-orange-600 p-5">
+    <div className="dados pb-10 bg-orange-600 p-10">
       <div className="max-w-4xl m-auto flex justify-between flex-col gap-5 md:flex-row">
         <div className="border p-10  flex flex-col gap-5 items-center text-center text-2xl uppercase rounded-lg text-white drop-shadow-lg w-full">
           <UsersFour size={128} color="#fff" weight="fill" />
