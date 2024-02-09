c9c037a8eee77e2b3c50ff77d0e6376a9d710682
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2024-02-09 13:05:48 -0300

    update paddings

--- src/app/sections/Atendimento.tsx
+++ src/app/sections/Atendimento.tsx
@@ -3,17 +3,17 @@ import location from '/src/app/assets/location.jpeg';
 
 export default function Atendimento(props: any) {
   return (
-    <div className="atendimento  pt-5 ">
-      <div className="local max-w-4xl m-auto text-center pb-10">
+    <div className="atendimento pt-5 ">
+      <div className="local max-w-4xl m-auto text-center pb-10 px-5 md:px-0">
         <h1 className="uppercase text-2xl pb-5">Área de abrangência</h1>
-        <h2 className="pb-10 px-5 text-zinc-700 text-justify md:text-center">
+        <h2 className="pb-10 text-zinc-700 text-justify md:text-center">
           Estamos empenhados ao máximo para expandir nossa cobertura e atender
           toda a região Norte do Brasil. Atualmente, já marcamos presença na
           região metropolitana de Belém e estamos trabalhando incansavelmente
           para levar nossos serviços a cada vez mais localidades, garantindo
           acesso e comodidade aos nossos clientes em toda a vasta região norte.
         </h2>
-        <div className="image overflow-hidden rounded-xl px-5 md:px-0 md:rounded-xl">
+        <div className="w-full rounded-lg overflow-hidden">
           <Image src={location} alt="Logo" quality={50} />
         </div>
       </div>
