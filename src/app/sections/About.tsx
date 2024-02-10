d509e9677b454d0fc73bfc2e288f86af23d1323a
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2024-02-10 09:10:30 -0300

    update about

--- src/app/sections/About.tsx
+++ src/app/sections/About.tsx
@@ -10,15 +10,14 @@ export default function About(props: any) {
         ultra.log.br
       </h1>
       <p className="text-justify p-5 mb-5 md:p-0">
-        Com uma abordagem pioneira, a{' '}
-        <span className="text-orange-600 font-semibold">ultra.log.br</span> é
-        constituído por especialistas com vasto conhecimento em logística.
-        Nossas atividades são planejadas para garantir praticidade e proteção ao
-        processo, buscando constantemente otimizar os custos e a rentabilidade
-        para todas as partes envolvidas - Embarcadores e Operadores. Como
-        provedor de serviços logísticos, trabalhamos em duas áreas principais:
-        atendimento direto aos clientes e distribuição de última milha para
-        outros operadores logísticos.
+        A <span className="text-orange-600 font-semibold">ultra.log.br</span>{' '}
+        conta com uma equipe de especialistas com grande experiência em
+        logística. Planejamos nossas atividades para tornar o processo prático e
+        seguro, sempre com o objetivo de reduzir custos e aumentar a
+        rentabilidade tanto para Embarcadores quanto para Operadores. Como
+        fornecedores de serviços logísticos, nos focamos principalmente em dois
+        aspectos: atendimento direto aos clientes e distribuição de última milha
+        para outros operadores logísticos.
       </p>
       <div className="about pb-10">
         <div className="flex justify-between flex-col gap-5 md:flex-row">
@@ -26,8 +25,8 @@ export default function About(props: any) {
             <Target size={96} color="#ea580c" weight="fill" />
             <h1 className="uppercase text-2xl text-orange-600">missão</h1>
             <p className="text-zinc-600">
-              Garantir a eficiência na movimentação e entrega de mercadorias,
-              superando expectativas.
+              Nosso objetivo é tornar a movimentação e entrega de mercadorias
+              eficiente, sempre buscando superar as expectativas.
             </p>
           </div>
           <div className=" p-10  flex flex-col gap-5 items-center text-center rounded-lg drop-shadow-lg  w-full ">
@@ -35,8 +34,8 @@ export default function About(props: any) {
             <h1 className="uppercase text-2xl text-orange-600">visão</h1>
 
             <p className="text-zinc-600">
-              Tornar-se líder regional, oferecendo soluções inovadoras e
-              sustentáveis para cadeias de suprimentos.
+              Nosso objetivo é ser líder regional, trazendo soluções criativas e
+              sustentáveis para as cadeias de suprimentos.
             </p>
           </div>
           <div className=" p-10  flex flex-col gap-5 items-center text-center rounded-lg drop-shadow-lg  w-full ">
@@ -44,8 +43,8 @@ export default function About(props: any) {
             <h1 className="uppercase text-2xl text-orange-600">valores</h1>
 
             <p className="text-zinc-600">
-              Integridade, excelência, colaboração, inovação, responsabilidade
-              social e compromisso com a satisfação do cliente.
+              Integridade, qualidade, trabalho em equipe, inovação,
+              responsabilidade social e foco na satisfação do cliente.
             </p>
           </div>
         </div>
